427b41b35e30b3a295d6e4e8dc2336905cb23d66
raffi23 <raffi@rhymecode.net>
2025-06-29 10:48:30 +0400

    remove: dist

--- dist/src/lib/axios.d.ts
+++
@@ -1,4 +0,0 @@
-import base from "axios";
-declare const axios: base.AxiosInstance;
-export default axios;
-//# sourceMappingURL=axios.d.ts.map
--- dist/src/routes/ace-routes.d.ts
+++
@@ -1,3 +0,0 @@
-declare const aceRouter: import("express-serve-static-core").Router;
-export default aceRouter;
-//# sourceMappingURL=ace-routes.d.ts.map

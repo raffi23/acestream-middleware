7096a109e56cd3eb92aa9cebf724497313d8d034
raffi23 <raffi@rhymecode.net>
2025-06-30 20:04:21 +0400

    remove: unused vars

--- apps/web/globlas.d.ts
+++ apps/web/globlas.d.ts
@@ -4,9 +4,6 @@ declare global {
   namespace NodeJS {
     interface ProcessEnv {
       NEXT_PUBLIC_BE_URL: string;
-      NEXT_PUBLIC_ACCESS_TOKEN: string;
-      NEXT_PUBLIC_ACCESS_TOKEN_ID: string;
-      NEXT_PUBLIC_ACCESS_TOKEN_COMBINED: string;
     }
   }
 }

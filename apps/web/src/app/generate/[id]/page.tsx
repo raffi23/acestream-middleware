bd552fd2520e1e74a4a53ec8f7288de7cd1d74e1
Raffi Chamakian <raffi@rhymecode.net>
2025-07-24 21:48:12 +0400

    chore :add local switch on generate

--- apps/web/src/app/generate/[id]/page.tsx
+++ apps/web/src/app/generate/[id]/page.tsx
@@ -1,13 +1,25 @@
+import LocalSwitchButton from "@/app/_components/local-switch-button";
 import ChannelItem from "@/components/channel-item";
 import { PropsWithParams } from "@/types";
 import { redirect } from "next/navigation";
 import { FC } from "react";
 
-const Page: FC<PropsWithParams> = async ({ params }) => {
+const Page: FC<PropsWithParams> = async ({ params, searchParams }) => {
   const { id } = await params;
   if (!id) redirect("/generate");
+  const { local = "false" } = await searchParams;
+  const isLocal = local === "true";
 
-  return <ChannelItem channel={{ infohash: id, name: "Generated channel" }} />;
+  return (
+    <div className="flex flex-col gap-4">
+      <ChannelItem
+        channel={{ infohash: id, name: "Generated channel" }}
+        isLocal={isLocal}
+      />
+
+      <LocalSwitchButton />
+    </div>
+  );
 };
 
 export default Page;

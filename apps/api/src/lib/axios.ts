5e7647156ac72f3c0dfecf4483c5568ded89f1a0
Raffi Chamakian <raffi@rhymecode.net>
2025-07-29 20:05:56 +0400

    fix: increase axios timeout

--- apps/api/src/lib/axios.ts
+++ apps/api/src/lib/axios.ts
@@ -2,6 +2,7 @@ import base from "axios";
 
 const axios = base.create({
   baseURL: process.env.ACE_URL,
+  timeout: 12000,
 });
 
 export { base as axiosBase };
--- apps/api/src/lib/scrape.ts
+++ apps/api/src/lib/scrape.ts
@@ -47,7 +47,7 @@ export const scrapeChannels = async () => {
     } catch (error) {
       console.error(
         `Error searching channels for query: ${query}`,
-        error,
+        (error as AxiosError).code,
         (error as AxiosError).message,
         (error as AxiosError).response?.status,
         (error as AxiosError).response?.data

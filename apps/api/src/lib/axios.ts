88dbba83ab7e2c64af266755a9c7604cee7bc80a
Raffi Chamakian <raffi@rhymecode.net>
2025-07-29 20:10:07 +0400

    fix: abort timeout

--- apps/api/src/lib/axios.ts
+++ apps/api/src/lib/axios.ts
@@ -3,6 +3,7 @@ import base from "axios";
 const axios = base.create({
   baseURL: process.env.ACE_URL,
   timeout: 12000,
+  signal: AbortSignal.timeout(12000),
 });
 
 export { base as axiosBase };

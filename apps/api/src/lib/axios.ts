bdfbaa086b2c5dc53f96f4bba90be0c2d44d89b3
Raffi Chamakian <raffi@rhymecode.net>
2025-08-17 01:17:23 +0400

    remove: abort signals

--- apps/api/src/lib/axios.ts
+++ apps/api/src/lib/axios.ts
@@ -2,15 +2,8 @@ import base from "axios";
 
 const axios = base.create({
   baseURL: process.env.ACE_URL,
-  timeout: 12000,
-  signal: AbortSignal.timeout(12000),
 });
 
-const axiosBase = base.create({
-  timeout: 20000,
-  signal: AbortSignal.timeout(20000),
-});
-
-export { axiosBase };
+export { base as axiosBase };
 
 export default axios;
